99c0d9489acfae9df92a7c37d96e9a4efe2317b8
vinay <vinaybarrenkula@gmail.com>
2026-05-21 14:16:14 +0530

    Delete WEEK-6/WEEK-1 directory

--- WEEK-6/WEEK-1/BIG_NUMBER.js
+++
@@ -1,8 +0,0 @@
-let a = 10;
-let b = 20;
-
-if (a > b) {
-    console.log("Big number is:", a);
-} else {
-    console.log("Big number is:", b);
-}
--- WEEK-6/WEEK-1/SUM_MARKS.jS
+++
@@ -1,8 +0,0 @@
-let marks = [90, 78, 65, 98];
-let sum = 0;
-
-for (let i = 0; i < marks.length; i++) {
-    sum += marks[i];
-}
-
-console.log("Sum of marks:", sum);
