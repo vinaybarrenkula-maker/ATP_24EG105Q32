8474eaf7c89367b0c1aec3ddf3a6e0ac63bcb556
vinay <vinaybarrenkula@gmail.com>
2026-05-21 14:18:01 +0530

    Delete WEEK-6/WEEK-2 directory

--- WEEK-6/WEEK-2/TASK MANAGEMENT/app.js
+++
@@ -1,6 +0,0 @@
-import {addTask, getAllTasks} from './task.js';
-addTask("Reading","high","2026-02-27")
-addTask("Writing","low","2026-02-28")
-addTask("Running","medium","2026-02-25")
-addTask("Eating","high","2026-03-01")
-getAllTasks()
